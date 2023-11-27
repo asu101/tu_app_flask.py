4e1a8f85cbd19849bee8e7f541b16ee0c79efbcd
Cristina <cristina.garcia39@alu.umh.es>
2023-11-27 19:56:48 +0100

    implement checkConnection

--- conexion_sql.py
+++ conexion_sql.py
@@ -1,17 +1,28 @@
+from flask import Flask, jsonify
 import psycopg2
 
-cnx = None
-try:
-    cnx = psycopg2.connect(
-        user="obqiwdgkne", 
-        password="41ZFD4JF1CA2MZ5J$",
-        host="idrhafitbit-server.postgres.database.azure.com", 
-        port=5432, 
-        database="idrhafitbit-database"
-    )
-    print("Conexión establecida con éxito.")
-except Exception as e:
-    print(f"Error al conectar a la base de datos: {e}")
-finally:
-    if cnx:
-        cnx.close()
+app = Flask(__name__)
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    # Aquí puedes agregar la lógica para obtener datos de la base de datos
+    # Por ahora, vamos a devolver un mensaje simple para probar
+    return jsonify({"message": "Datos obtenidos con éxito"})
+
+@app.route('/checkConnection', methods=['GET'])
+def check_connection():
+    try:
+        # Reemplaza con tus propios detalles de conexión
+        conn = psycopg2.connect(
+            host="idrhafitbit-server.postgres.database.azure.com",
+            dbname="idrhafitbit-database",
+            user="obqiwdgkne",
+            password="41ZFD4JF1CA2MZ5J$"
+        )
+        conn.close()
+        return jsonify({"connection": "successful"})
+    except Exception as e:
+        return jsonify({"connection": "failed", "error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
